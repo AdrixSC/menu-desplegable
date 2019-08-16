f992714a02b5594e707a9de1f1c61b00bc2a19fd
Adriana <adriana.be@outlook.com>
2019-08-15 22:44:58 -0500

    agregando estilos

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 $(document).ready(function () {
     $('.boton-menu').hover(function () {
         $('#menu-dropdown').slideDown();
-        $('ul li:has(ul)').hover(function(e) {
-             $(this).find('ul').css({display: "block"});
-         },
-         function(e) {
-             $(this).find('ul').css({display: "none"});
-         });
+        $('ul li:has(ul)').hover(function (e) {
+            $(this).find('ul').css({ display: "block" });
+        },
+            function (e) {
+                $(this).find('ul').css({ display: "none" });
+            });
     });
-    $("*:not(.boton-menu, #menu-dropdown)").on("click", function(){
-      if ($("#menu-dropdown").is(":visible")){
-        $("#menu-dropdown").slideUp();
-      }
+    $("*:not(.boton-menu, #menu-dropdown)").on("click", function () {
+        if ($("#menu-dropdown").is(":visible")) {
+            $("#menu-dropdown").slideUp();
+        }
     });
 });
