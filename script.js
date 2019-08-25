4890bcc8700c32d6fd5b5da88e2eb5811eba4a4e
Arisbeth Martínez <arisbeth@MacBook-Air-de-Arisbeth.local>
2019-08-25 18:53:55 -0500

    ajustes

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
-/*$(document).ready(function () {
-    $('.boton-menu').hover(function () {
-        $('#menu-dropdown').slideDown();
-        $('ul li:has(ul)').hover(function (e) {
-            $(this).find('ul').css({ display: "block" });
-        },
-            function (e) {
-                $(this).find('ul').css({ display: "none" });
-            });
-    });
-    $("*:not(.boton-menu, #menu-dropdown)").on("click", function () {
-        if ($("#menu-dropdown").is(":visible")) {
-            $("#menu-dropdown").slideUp();
-        }
-    });
-});*/
+$(document).ready(function() {
+  $(".boton-menu").hover(function() {
+    $("#menu-dropdown").slideDown();
+    $("#menu-dropdown").addClass("active");
+    $("ul li:has(ul)").hover(
+      function(e) {
+        $(this)
+          .find("ul")
+          .css({ display: "block" });
+      },
+      function(e) {
+        $(this)
+          .find("ul")
+          .css({ display: "none" });
+      }
+    );
+  });
+  $("*:not(.boton-menu, #menu-dropdown)").on("click", function() {
+    if ($("#menu-dropdown").is(":visible")) {
+      $("#menu-dropdown").slideUp();
+      $("#menu-dropdown").removeClass("active");
+    }
+  });
+});
 
-$(document).ready(main);
+/*$(document).ready(main);
 
 var contador = 1;
 
@@ -33,4 +41,4 @@ function main() {
     $('.deptos').click(function () {
         $(this).children('.subdeptos').slideToggle();
     })
-}
+}*/
